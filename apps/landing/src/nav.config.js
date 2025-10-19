a5efb9e10aaeb346aaa9aac0e40ccaadd15e5637
eserafasi <rafaforeroa@gmail.com>
2025-10-19 11:52:38 -0500

    Add accessible navigation bar for landing app

---
+++ apps/landing/src/nav.config.js
@@ -0,0 +1,33 @@
+const navigation = [
+  {
+    title: 'Agenda',
+    route: '/',
+  },
+  {
+    title: 'Talleres',
+    route: 'https://impro.world/agendataller',
+    external: true,
+  },
+  {
+    title: 'Registro',
+    route: 'https://impro.world/registro',
+    external: true,
+  },
+  {
+    title: 'FAQ',
+    route: 'https://impro.world/faq',
+    external: true,
+  },
+  {
+    title: 'Transmisión',
+    route: 'https://impro.world/transmision',
+    external: true,
+  },
+  {
+    title: 'Sobre impro.world',
+    route: 'https://impro.world/about-impro',
+    external: true,
+  },
+];
+
+export default navigation;
